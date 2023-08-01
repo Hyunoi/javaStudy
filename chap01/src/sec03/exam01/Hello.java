e10b37cec9ff19754b8ea97e4388d587ebd4e266
hyunoi <nhe0622@gmail.com>
2023-08-01 20:14:04 +0900

    chap01. 자바 시작하기

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,12 @@
+package sec03.exam01;
+
+/*
+작성자: 노현이
+작성일자: 2023.08.01
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}
